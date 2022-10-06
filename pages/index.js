dd2cf0b1993a2b2e50aa6981e6955db417962272
simba-fs <simba.fs@gmail.com>
2022-10-07 00:21:18 +0800

    keyboard event

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from "next/head";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+	const [keys, setKeys] = useState("");
+	useEffect(() => {
+		const keydownHandler = (e) => {
+			console.log(e.keyCode);
+			if (e.keyCode == 13) setKeys("");
+			else setKeys((keys) => (keys += e.key));
+		};
+		document.addEventListener("keydown", keydownHandler);
+		return () => {
+			document.removeEventListener("keydown", keydownHandler);
+		};
+	}, []);
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -12,55 +24,20 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>
-					嘸蝦米打字練習
-				</h1>
+				<h1 className={styles.title}>嘸蝦米打字練習</h1>
 
-				<p className={styles.description}>
-					互動式打字練習
-				</p>
+				<p className={styles.description}>互動式打字練習</p>
 
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/canary/examples"
-						className={styles.card}
-					>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
+				<h1 className={styles.title}>{keys}</h1>
 			</main>
 
 			<footer className={styles.footer}>
 				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					href="https://github.com/simbafs/boshiamy-exercise"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Powered by{" "}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
+					Build by SimbaFs with &hearts;
 				</a>
 			</footer>
 		</div>
