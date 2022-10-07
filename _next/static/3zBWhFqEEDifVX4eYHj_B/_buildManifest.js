33f51c49f8436b6f10af09367d6a1807ed3a5dee
simbafs <simbafs@users.noreply.github.com>
2022-10-07 08:52:40 +0000

    deploy: d0fce21917a2636f75d3525bd32de16c7b3583b4

---
+++ _next/static/3zBWhFqEEDifVX4eYHj_B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d3b29e9e4c3033f0.css","static/chunks/pages/index-a750b6c0de312e45.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
